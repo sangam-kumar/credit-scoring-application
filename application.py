88b0033fd0f0cd025a698a8a8e8af29fea284ae7
Sangam Kumar <53684372+sangam-kumar@users.noreply.github.com>
2023-03-29 21:42:42 +0530

    Update application.py

--- application.py
+++ application.py
@@ -36,6 +36,7 @@ try:
     with col2:
         st.image(img1, caption = "Credit Risk Analysis")
         st.markdown('[PROJECT TARS](https://github.com/sangam-kumar)')
+        st.markdown('[Under Esteemed Guidance of Prof. Dr. Ravichandran Sivaramakrishnan](https://github.com/sangam-kumar)')
 
 
 except:
